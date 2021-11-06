b052a5dfa6e4fc24ed2b93ca7042f08b19a30be0
Erick Crager <erick@youhavethewrong.info>
2021-11-06 18:35:07 -0500

    Replicate want-worn-wear as a babashka script.

--- script/wornwear_cli/main.clj
+++ script/wornwear_cli/main.clj
@@ -1,23 +1,39 @@
 (ns wornwear-cli.main
-  (:require [babashka.curl :as curl]))
+  (:require [babashka.curl :as curl]
+            [cheshire.core :as json]
+            [spartan.spec] ;; supply clojure.spec.alpha for venia
+            [venia.core :as v]))
 
 (def search-url "https://reware-production.yerdlesite.com/v4/graphql")
 (defn build-item-url
   [id color]
   (format "https://wornwear.patagonia.com/shop/mens/%s/%s" id color))
+(def ww-query
+  (v/graphql-query {:venia/operation {:operation/type :query
+                                      :operation/name "initShop"}
+                    :venia/queries   [{:query/data  [:partner {:uuid "7d32ad83-330e-4ccc-ba03-3bb32ac113ac"}
+                                                     [[:shop {:slug "mens"}
+                                                       [[:browse {:limit 100 :sort ""
+                                                                  :filters [{:name "M" :tag "size"}]}
+                                                         [[:items [:availableSizes :color :displayColor
+                                                                   :price :title
+                                                                   [:pdpLink [:path :url]]]]]]]]]]}]}))
 
+(def standard-params
+  {:body (json/generate-string {:query ww-query})
+   :headers {"Accept-Language" "en-US,en;q=0.5"
+             "Content-Type" "application/json"
+             "Referer" "https://wornwear.patagonia.com/shop/mens"
+             "origin" "https://wornwear.patagonia.com"
+             "DNT" "1"
+             "Connection" "keep-alive"}})
 
-(defn get-url [url]
-  (println "Downloading url:" url)
-  (curl/get url))
+(defn post-search
+  [url]
+  (:body (curl/post url standard-params)))
 
-(defn write-html [file html]
-  (println "Writing file:" file)
-  (spit file html))
-
-(defn -main [& args]
-  (let [[url file] args]
-    (when (or (empty? url) (empty? file))
-      (println "Usage: <url> <file>")
-      (System/exit 1))
-    (write-html file (:body (get-url url)))))
+(defn -main [& _args]
+  (println "Posting search")
+  (let [data (json/parse-string (post-search search-url) true)]
+    (doseq [item (:items (get-in data [:data :partner :shop :browse]))]
+      (println item))))
